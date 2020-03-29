e7a4746ca6134386db7cfe146cec10bb5b729e55
Μιλτος Τσακιρης <miltostsalekkas@gmail.com>
2020-03-29 19:16:49 +0100

    Added Mouse Motion Event for Chrome

--- sketch.js
+++ sketch.js
@@ -1,41 +1,43 @@
 /*
-	Check it out:
-	https://p5js.org/examples/sound-mic-input.html
+	Microphone Visualization First Iteration
 */
 
 var mic;
 var sensibility;
 
-function setup() {
-  createCanvas(windowWidth,windowHeight);
 
-	sensibility=4;
-	
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  sensibility = 4;
+
+  getAudioContext().suspend();
   // Crea el Audio input
   mic = new p5.AudioIn();
-	//Inicia el Audio Input
+  //Inicia el Audio Input
   mic.start();
 }
 
 function draw() {
   background('#F7D488');
-	
-	noStroke();
-	fill(255);
-	textSize(20);
-	// text('USE THE MICROPHONE', 52, 30);
 
-  // Obtiene el volumen (entre 0 y 1.0)
+  noStroke();
+  fill(255);
+  textSize(20);
+
+
   var volume = mic.getLevel();
-  
-	//redefine el rango de vol (entre 0 y 1.0) en el rango entre -> 50 y 400
 
-  var heightAux1 = map(volume*sensibility, 0, 1, 50,400);
-	// restringe el valor de h entre -> 25 y height -25
-	//var heightAux2 = constrain(heightAux1, 50 , height-25);
-	
-	noFill();
-	strokeWeight(heightAux1/8);
-	stroke('#A37B73');
-	ellipse(width/2, height/2 , 100+heightAux1/8, 100+heightAux1/8);
+
+  var heightAux1 = map(volume * sensibility, 0, 1, 50, 400);
+ 
+  noFill();
+  strokeWeight(heightAux1 / 8);
+  stroke('#A37B73');
+  ellipse(width / 2, height / 2, 100 + heightAux1 / 8, 100 + heightAux1 / 8);
+
+}
+
+function mouseMoved() {
+  userStartAudio();
 }
